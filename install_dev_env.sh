6b1c66845d8153622bf5b3d486971eec180be8e4
Hadestructhor <angelo_alyacoub@hotmail.fr>
2023-10-20 16:51:45 +0200

    Added missing nvm install

--- install_dev_env.sh
+++ install_dev_env.sh
@@ -100,6 +100,10 @@ echo 'Installing Python 3...'
 sudo apt install python3
 
 echo 'Installing nvm and latest node version...'
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 nvm install stable
 nvm use stable
 
